f258a86ca2f5d2697166f617b256175539f393d1
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2024-11-14 12:28:38 +0530

    added images

---
+++ src/components/ticTacToe/ticTacToe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ticTacToe.css'
+import circleIcon from '../assets/Circle.png'
+import squareIcon from '../assets/Square.png'
+const ticTacToe = () => {
+  return (
+    <div>ticTacToe</div>
+  )
+}
+
+export default ticTacToe
